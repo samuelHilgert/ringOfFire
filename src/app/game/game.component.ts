8dec3a1c5b3cd7b5e9e937f9de5ed15579c7d7d3
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-08-05 15:50:29 +0200

    updated

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  takeCardAnimation = false;
 
+  takeCard() {
+    this.takeCardAnimation = true;
+  }
 }
