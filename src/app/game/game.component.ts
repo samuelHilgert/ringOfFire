28b1cca851be5bca29216c1b11d8b6e9774407e8
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-08-05 17:59:45 +0200

    array added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,17 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   takeCardAnimation = false;
+  currentCard: string | undefined = '';
+  game: Game = new Game();
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   takeCard() {
-    this.takeCardAnimation = true;
+    if (!this.takeCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log('New Card: ', this.currentCard);
+      console.log('Game is ',  this.game);
+      this.takeCardAnimation = true;
+      if(this.currentCard != undefined) {
+        this.game.playedCards.push(this.currentCard);
+      }
+      setTimeout(() => {
+        this.takeCardAnimation = false;
+      }, 2000);
+    }
   }
 }
